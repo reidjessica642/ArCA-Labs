cbc1164e962829a2fb8d09afc4c8e1bedc6eb9ac
reidjessica642 <reidjessica642@gmail.com>
2026-05-15 20:42:55 -0500

    Following original steps, will edit inner contents later on

---
+++ Lab4/controllers/cves.controller.js
@@ -0,0 +1,10 @@
+import { ChickensService } from '../services/chickens.service.js';
+
+export class ChickensController {
+  static getChickens = (req, res) => {
+    console.log('ChickensController : getChickens()');
+    
+    const result = ChickensService.getChickens()
+    res.status(200).json(result);
+  };
+}
---
+++ Lab4/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { ChickensController } from './controllers/cves.controller.js';
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/api/v1/chickens', ChickensController.getChickens);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
