a56a2f3cdc5a06fb28cd39f6714ccdb23f3c4201
reidjessica642 <reidjessica642@gmail.com>
2026-05-22 16:19:47 -0500

    Added unique IDs to hardcoded info

--- Lab4/repositories/cves.repository.js
+++ Lab4/repositories/cves.repository.js
@@ -1,8 +1,8 @@
 import { logger } from '../utils/logger.js';
 
-let CVES = [ //TODO: create actual uuids for mock data
+let CVES = [
   {
-    id: '1',
+    id: 'bac705c6-dbcb-4512-ad71-ee0dd765b7c7',
     name: 'CVE-2026-24216',
     url: 'https://nvd.nist.gov/vuln/detail/CVE-2026-24216',
     vulnType: [
@@ -14,7 +14,7 @@ let CVES = [ //TODO: create actual uuids for mock data
     color: 'Red'
   },
   {
-    id: '2',
+    id: 'd27005c0-6d7b-40a8-bd2b-7799cdd78138',
     name: 'CVE-2026-6960',
     url: 'https://app.opencve.io/cve/CVE-2026-6960',
     vulnType: [
@@ -24,7 +24,7 @@ let CVES = [ //TODO: create actual uuids for mock data
     color: 'Red'
   },
   {
-    id: '3',
+    id: '973bab5d-d9a2-484d-a728-0aeb5024f5c9',
     name: 'CVE-2026-45232',
     url: 'https://nvd.nist.gov/vuln/detail/CVE-2026-45232',
     vulnType: [
